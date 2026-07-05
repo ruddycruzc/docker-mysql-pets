37e300f4f6dcd9e5c3140ed494c6de4f446136af
Ruddy <ruddy391@gmail.com>
2026-07-06 01:36:29 +0200

    feat: add database creation and initialization scripts

---
+++ sql/03_insert_data.sql
@@ -0,0 +1,17 @@
+USE pets_db;
+
+INSERT INTO pets (name, animal_type, breed, age)
+VALUES
+('Max', 'Dog', 'Chihuahua', 2),
+('Luna', 'Cat', 'Siamese', 4),
+('Terry', 'Dog', 'Pitbul', 7),
+('Gala', 'Dog', 'Boder-Collie', 1),
+('Ricardo', 'Parakeet', 'Budgerigar', 3),
+('Rocky', 'Dog', 'Labrador Retriever', 5),
+('Coco', 'Parrot', 'African Grey', 3),
+('Nala', 'Cat', 'Maine Coon', 2),
+('Simba', 'Rabbit', 'Mini Lop', 1),
+('Thor', 'Dog', 'German Shepherd', 6),
+('Kiwi', 'Hamster', 'Syrian', 1),
+('Dory', 'Fish', 'Goldfish', 2),
+('Spike', 'Turtle', 'Red-Eared Slider', 8);
